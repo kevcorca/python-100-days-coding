9a316fe9637e178ece9767241a6e82a8adc3cb84
kevcorca <kevcorca@gmail.com>
2025-09-16 16:34:06 -0600

    Added keyword and positional arguments python

---
+++ Day08/keyword_positional_arguments.py
@@ -0,0 +1,18 @@
+# Functions with input
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+
+greet_with_name("Jack Bauer")
+
+
+
+def greet_with(name, location):
+    print(f"Hello {name}!")
+    print(f"What is like in {location}?")
+
+greet_with("Angela","England")
+
+greet_with(location="London", name="Yannis")
